6928160d903ada7344667d579ad5b4a1555c7ea6
Gavin0x0 <632891553@qq.com>
2023-11-21 22:26:57 +0800

    Fix windowManager.waitUntilReadyToShow() bug

--- lib/main.dart
+++ lib/main.dart
@@ -19,7 +19,7 @@ void main() async {
     skipTaskbar: false,
     titleBarStyle: TitleBarStyle.hidden,
   );
-  windowManager.waitUntilReadyToShow(windowOptions, () async {
+  await windowManager.waitUntilReadyToShow(windowOptions, () async {
     await windowManager.show();
     await windowManager.focus();
   });
